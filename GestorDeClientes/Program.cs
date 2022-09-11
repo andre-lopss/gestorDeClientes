d7a5bd8edac88f883aebd0a3eff6cc714a3cb6ef
andre-lopss <andrelneres@hotmail.com>
2022-09-11 18:14:38 -0300

    Criei o repositório e coloquei o menu

---
+++ GestorDeClientes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeClientes
+{
+
+    enum Menu { Listagem = 1, Adicionar, Remover, Sair}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+            
+            while (!escolheuSair)
+            {
+                Console.WriteLine("SISTEMA DE CLIENTES - BEM VINDO!");
+                Console.WriteLine("1-Listagem\n2-Adicionar\n3-Remover\n4-Sair");
+                int intOp = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intOp;
+
+                switch (opcao)
+                {
+                    case Menu.Listagem:
+                        break;
+                    case Menu.Adicionar:
+                        break;
+                    case Menu.Remover:
+                        break;
+                    case Menu.Sair:
+                        escolheuSair = true;
+                        break;
+                }
+                Console.Clear();
+            }
+           
+        }
+    }
+}
